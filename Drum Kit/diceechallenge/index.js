b17b22040a4607cad2902ab00de63d0ebec6f93e
Mohan1404 <92218509+Mohan1404@users.noreply.github.com>
2022-01-31 22:27:57 +0530

    Add initial revange website

---
+++ Drum Kit/diceechallenge/index.js
@@ -0,0 +1,17 @@
+
+function myFunction(){
+  var randomNumber1=Math.floor ( ( Math.random() ) * 6 ) +1;
+  var randomImageSource1="images/dice"+randomNumber1+".png";
+  document.querySelectorAll("img")[0].setAttribute("src",randomImageSource1);
+  var randomNumber2=Math.floor ( ( Math.random() ) * 6 ) +1;
+  var randomImageSource2="images/dice"+randomNumber2+".png";
+  document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+  if(randomNumber1>randomNumber2)
+  {
+    document.querySelector("h1").innerHTML="player 1 wins🚩";
+  }else if (randomNumber1==randomNumber2) {
+      document.querySelector("h1").innerHTML="both players win🚩";
+  }else {
+    document.querySelector("h1").innerHTML="player 2 wins🚩";
+}
+}
